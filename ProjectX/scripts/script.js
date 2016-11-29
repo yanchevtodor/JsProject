cb4d4f61acbbe13a716a756f50563ebf7d84d152
yanchevtodor <yanchev.todor@gmail.com>
2016-11-29 13:57:49 +0200

    a bit

--- ProjectX/scripts/script.js
+++ ProjectX/scripts/script.js
@@ -1,6 +1,9 @@
 function loadLogin() {
     $('#result').load("templates/login.html");
+    alert("l")
 }
 function loadRegister() {
     $('#result').load("templates/register.html");
+    alert("r")
+
 }
